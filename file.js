f169f29987251936a3c61584c351aaf8025f42f8
sanjay srinivasan <155130601+Srinivasan-1609@users.noreply.github.com>
2024-03-02 12:12:33 +0530

    bot

---
+++ file.js
@@ -0,0 +1,20 @@
+function chat(){
+    let quetions={
+        "hi":"hello :)",
+        "how are you":"Good :) You say?",
+        "i'm also good":"How can i help you!",
+        "who are you":"I'm codewithMuazim",
+        "your favourite language":"Java, but most people hate it :(",
+        "okay":"Thanku for being here!",
+        "bye":"Okay! see you soon... :)",
+        "i love you":"love you too..:)",
+    };
+    let user=document.getElementById("questionBox").value;
+    document.getElementById("answerBox").innerHTML=user + "<br>";
+    if(user in quetions){
+        document.getElementById("answerBox").innerHTML=quetions[user] +"<br>";
+    }
+    else{
+        document.getElementById("answerBox").innerHTML="sorry, its not in my database";
+    }
+}
